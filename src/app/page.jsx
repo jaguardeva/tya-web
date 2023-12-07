afe829868797b22c10a236b2f130bcc258617c48
jaguardeva <jaguardeva54@gmail.com>
2023-12-07 07:42:31 +0700

    done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -27,36 +27,6 @@ export default function HomePage() {
           </p>
 
           <div className="flex gap-5">
-            <a
-              href="https://www.instagram.com/sitimaftuhah654/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA=="
-              className="cursor-pointer"
-              target="_blank"
-            >
-              <FaInstagramSquare
-                className="text-3xl text-slate-900 md:text-4xl  hover:text-purple-600 transition-all ease-in-out duration-100"
-                title="Instagram"
-              />
-            </a>
-            <a href="" className="cursor-pointer">
-              <FaFacebookSquare
-                className="text-3xl text-slate-900 md:text-4xl  hover:text-purple-600 transition-all ease-in-out duration-100"
-                title="Facebook"
-              />
-            </a>
-            <a href="" className="cursor-pointer">
-              <FaTwitterSquare
-                className="text-3xl text-slate-900 md:text-4xl  hover:text-purple-600 transition-all ease-in-out duration-100"
-                title="Twitter"
-              />
-            </a>
-            <a href="" className="cursor-pointer">
-              <FaLinkedin
-                className="text-3xl text-slate-900 md:text-4xl  hover:text-purple-600 transition-all ease-in-out duration-100"
-                title="Linkedin"
-              />
-            </a>
-          </div>
-          <div className="flex gap-5">
             <Link
               href="/about"
               className="mt-6 flex justify-center items-center w-[40%]  md:w-[30%] py-3 px-5 text-sm md:text-xl font-semibold rounded-lg bg-purple-600 hover:bg-purple-700 focus:bg-purple-700 text-white transition-all ease-in-out duration-150"
